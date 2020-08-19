fff9a78c3e5eb84b2dd2ca7b7aa7eebb5c86363d
Darshil Papalkar <67659174+Darshil-Papalkar@users.noreply.github.com>
2020-08-20 00:52:31 +0530

    Update mail_list.py

--- mail_list.py
+++ mail_list.py
@@ -1,26 +1,30 @@
 email = dict()
-# edit by entering person name on left and mail id on right
-email['myself']='myself@gmail.com' # enter the mail id 
-email['father']='father@gmail.com' # enter the mail id
-email['mother']='mother@gmail.com' # enter the mail id
 
-def mail(K):
-    for k,v in email.items():
-        if K in k:
-            return(v)
+# Enter your receiver's name and his mail id in the dict
 
-def check_email(K):
-    for k,v in email.items():
-        if k==K:
-            return('True')
-            break    
+email['myself'] = 'myself@gmail.com'
+email['father'] = 'father@gmail.com'
+email['mother'] = 'mother@gmail.com'
+email['friend'] = 'friend@gmail.com'
+
+
+def mail(x):
+    """Returns the receivers name and his email id if present in list"""
+    for k, v in email.items():
+        if x == k:
+            return k, v
+
+
+def check_email(x):
+    """Checks whether the requested user is there in the list or not"""
+    for m, n in email.items():
+        if m == x:
+            return 'True'
+    return 'False'
 
 
 if __name__ == '__main__':
-    id = input()
-    mail(id)
-    check_email(id)
-
-
-
-
+    a, c = mail(input())
+    print(a, c)
+    b = check_email(input())
+    print(b)
