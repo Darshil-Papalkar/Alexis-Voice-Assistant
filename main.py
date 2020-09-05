9f07d89f8819b9b6d2a4e6ef23d49bb69b144869
Darshil Papalkar <67659174+Darshil-Papalkar@users.noreply.github.com>
2020-09-05 22:08:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,15 +37,11 @@ def translate(word):
                ' instead,  respond with Yes or No.'))
         speak('Did you mean ' + x +
               ' instead,  respond with Yes or No.')
-        ans = 'empty'
-        while ans == 'empty':
-            ans = command(takeCommand().lower())
-            if ans == 'exit':
-                exit()
-            else:
-                ans = 'empty'
+        ans = takeCommand().lower()
+        while ans is None:
             speak("We didn't understand your entry.")
             speak('Please state yes or no..')
+            ans = takeCommand().lower()
         if 'yes' in ans or 'ok' in ans:
             print(data[x])
         elif 'no' in ans:
@@ -60,9 +56,7 @@ def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-
     # enter file_path of password stored in text file
-
     file = open('file_path_of_text_file_of_password', 'r')
     server.login("sender's_main_id", file.readline())
     server.sendmail("sender's_mail_id", to, content)
@@ -128,9 +122,7 @@ if __name__ == "__main__":
                 exit()
 
         elif 'video song' in query:
-
             # enter the video songs folder path
-
             video_dir = 'video_song_folder_path'
             file = directory(video_dir)
             if file == 'None':
@@ -138,7 +130,7 @@ if __name__ == "__main__":
                 continue
 
         elif 'song' in query or 'music' in query:
-
+            
             # enter the songs folder path
 
             song_dir = 'music_folder_path'
@@ -149,9 +141,7 @@ if __name__ == "__main__":
 
 
         elif 'movie' in query:
-
             # enter the movies folder path
-
             movie_dir = 'movie_folder_path'
             file = directory(movie_dir)
             if file == 'None':
@@ -160,12 +150,10 @@ if __name__ == "__main__":
 
 
         elif 'web series' in query:
-
             # path_syntax = "path\web_series_folder\web_series_particular\web_series_season\web_series_episode"
             # example = "D:\Web Series\Sherlock Holmes\Sherlock Holmes S02\Episode-1"
             # enter the web series folder path
-
-            web_series_folder = 'path\web_series_folder_path'   # D:\Web Series
+            web_series_folder = 'path\web_series_folder_path'   # web_series_path = "D:\Web Series"
             file = directory(web_series_folder)
             if file == 'None':
                 speak('Going back to main program, Continuing')
@@ -176,9 +164,9 @@ if __name__ == "__main__":
             speak(f"Sir, the time is {strTime}")
 
         elif 'code' in query:
-
+            
             # enter the vs code application path
-
+            
             codePath = 'vs_code_path'
             while cmd == 'empty':
                 speak('Should i close the program while opening VS-Code Sir')
@@ -275,55 +263,6 @@ if __name__ == "__main__":
             elif cmd == 'exit':
                 exit()
 
-        elif 'github' in query:
-            while cmd == 'empty':
-                speak('Should i close the program while opening github Sir')
-                cmd = command(takeCommand())
-                if cmd == 'None' or cmd == 'exit':
-                    break
-                else:
-                    cmd = 'empty'
-                    continue
-
-            # enter the github link
-
-            webbrowser.get().open_new_tab('github_link')
-            if cmd == 'None':
-                speak('Going back to main program...Continuing')
-                continue
-            elif cmd == 'exit':
-                exit()
-
-        elif 'linkedin' in query:
-            while cmd == 'empty':
-                speak('Should i close the program while opening linkedin Sir')
-                cmd = command(takeCommand())
-                if cmd == 'None' or cmd == 'exit':
-                    break
-                else:
-                    cmd = 'empty'
-                    continue
-
-            # enter linkedin account link
-
-            webbrowser.get().open_new_tab('linkedin_link')
-            if cmd == 'None':
-                speak('Going back to main program...Continuing')
-                continue
-            elif cmd == 'exit':
-                exit()
-
-        elif 'dictionary' in query:
-            while find == 'empty':
-                speak('What do you want me to search in dictionary ? ')
-                find = command(takeCommand())
-            if find == 'None':
-                speak('Going back to main program...Continuing')
-                continue
-            elif find == 'exit':
-                exit()
-            translate(find)
-
         elif 'mail' in query:
             C = 'False'
             completed = False
@@ -372,7 +311,7 @@ if __name__ == "__main__":
         elif 'how are you' in query:
             speak('I am fine sir, Good to see you back')
 
-        elif 'master' in query:
+        elif 'your master' in query:
             speak('Darshil, is my master. He created me...')
 
         elif 'your name' in query or 'who are you' in query:
@@ -386,6 +325,51 @@ if __name__ == "__main__":
                 'Alexis is a given name derived from several saints venerated by the Eastern Orthodox and Roman '
                 'Catholic churches, including Saint Alexis of Rome. Alexis means "helper, defender".')
 
+        elif 'github' in query:
+            while cmd == 'empty':
+                speak('Should i close the program while opening github Sir')
+                cmd = command(takeCommand())
+                if cmd == 'None' or cmd == 'exit':
+                    break
+                else:
+                    cmd = 'empty'
+                    continue
+            # enter the github link
+            webbrowser.get().open_new_tab('https://github.com/Darshil-Papalkar')
+            if cmd == 'None':
+                speak('Going back to main program...Continuing')
+                continue
+            elif cmd == 'exit':
+                exit()
+
+        elif 'linkedin' in query:
+            while cmd == 'empty':
+                speak('Should i close the program while opening linkedin Sir')
+                cmd = command(takeCommand())
+                if cmd == 'None' or cmd == 'exit':
+                    break
+                else:
+                    cmd = 'empty'
+                    continue
+            # enter linkedin account link
+            webbrowser.get().open_new_tab('https://www.linkedin.com/in/darshil-papalkar-923016194/')
+            if cmd == 'None':
+                speak('Going back to main program...Continuing')
+                continue
+            elif cmd == 'exit':
+                exit()
+
+        elif 'dictionary' in query:
+            while find == 'empty':
+                speak('What do you want me to search in dictionary ? ')
+                find = command(takeCommand())
+            if find == 'None':
+                speak('Going back to main program...Continuing')
+                continue
+            elif find == 'exit':
+                exit()
+            translate(find)
+
         elif 'news' in query:
             speak('Sure sir...')
             speak_news()
@@ -397,31 +381,29 @@ if __name__ == "__main__":
         elif 'exit' in query:
             exit()
 
-        elif 'what can you perform' in query:
+        elif 'what can you do' in query:
             print(
                 ' 1. search wikipedia -- to search in wikipedia\n' +
                 ' 2. open youtube -- to open youtube on ms edge\n' +
                 ' 3. open google -- to open google on ms edge\n' +
                 ' 4. email -- to send email \n' +
-                ' 5. open code -- to open vs code\n' +
-                ' 6. github -- to open the github\n' +
-                ' 7. linkedin -- to open linkedin account\n' +
-                ' 8. dictionary -- to search for in dictionary\n' +
-                ' 9. news -- to seach the current news\n' +
-                '10. play video songs -- to open video song list\n' +
-                '11. play web series -- to open webseries folder\n' +
-                '12. play movie -- to open movie list\n' +
-                '13. play song -- to play songs'
-                '14. find on youtube -- to search on youtube\n' +
-                '15. time -- to display the current time\n' +
-                '16. search -- to search on ms edge\n' +
-                '17. find location -- to search the user choice location\n' +
-                '18. how are you -- to know the current condition\n' +
-                '19. your master name -- to know my master\n' +
-                '20. your name -- to know my name\n' +
-                "21. stands for -- to know my name's full form\n" +
-                '22. back -- to go back to the main program\n' +
-                '23. exit -- to exit this program at any time\n'
+                ' 5. play video songs -- to open video song list\n' +
+                ' 6. play web series -- to open webseries folder\n' +
+                ' 7. play movie -- to open movie list\n' +
+                ' 8. play song -- to play songs'
+                ' 9. find on youtube -- to search on youtube\n' +
+                '10. time -- to display the current time\n' +
+                '11. search -- to search on ms edge\n' +
+                '12. find location -- to search the user choice location\n' +
+                '13. your master -- to know my master\n' +
+                '14. your name -- to know my name\n' +
+                "15. stands for -- to know my name's full form\n" +
+                '16. open code -- to open vs code\n' +
+                '17. github -- to open the github\n' +
+                '18. linkedin -- to open linkedin account\n' +
+                '19. dictionary -- to search for in dictionary\n' +
+                '20. news -- to seach the current news\n' +
+                '21. exit -- to exit this program\n'
             )
             speak('Sir, I can perform tasks stated below')
 
